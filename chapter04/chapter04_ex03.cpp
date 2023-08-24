192316f924df0b59a60f3b81cd3655adc1ad6bce
mb305151 <115078322+mb305151@users.noreply.github.com>
2023-08-24 12:56:09 +0200

    Add files via upload

---
+++ chapter04/chapter04_ex03.cpp
@@ -0,0 +1,42 @@
+﻿
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<double> allDistances;
+    double distance = 0;
+    double total = 0;
+    double smallest = 0;
+    double largest = 0;
+    double largestDist = 0;
+    double smallestDist = 0;
+
+    while (cin >> distance) {
+        allDistances.push_back(distance);                          // loading all distances from cin
+    }
+    smallestDist = allDistances[0];
+    largestDist = allDistances[0];
+
+
+
+    for (int i = 0; i < allDistances.size(); i++) {
+
+        total += allDistances[i];                                             // adding up distances for total
+       
+    }
+    for (int i = 0; i < allDistances.size() - 1; i++) {                     
+        if (allDistances[i] + allDistances[i+1] > largestDist) {            // searching largest distance
+            largestDist = allDistances[i] + allDistances[i+1];
+        }
+        if (allDistances[i] + allDistances[i+1] < smallestDist) {           // searching smallest distance
+            smallestDist = allDistances[i] + allDistances[i+1];
+        }
+    }
+    cout << "Total Distance: " << total << endl;
+    cout << "Largest Distance: " << largestDist << endl;
+    cout << "Smallest Distance: " << smallestDist << endl;
+
+    }
+
