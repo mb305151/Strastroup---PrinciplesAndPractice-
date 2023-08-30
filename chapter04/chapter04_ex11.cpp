ba65a66587e2318d1151772a753f4f2dd33ff9a6
mb305151 <115078322+mb305151@users.noreply.github.com>
2023-08-30 15:19:52 +0200

    Add files via upload

---
+++ chapter04/chapter04_ex11.cpp
@@ -0,0 +1,24 @@
+﻿//PRIMES 2,3,5,7 ETC.
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> prime;
+
+bool is_prime(int n)
+{
+	for (int i = 0; i < prime.size(); i++)
+		if (n % prime[i] == 0) return false;	
+	return true;	
+}
+int main()
+{
+	prime.push_back(2);	// consider the smallest prime
+
+	for (int i = 3; i <= 100; i++)	// test all integers [3:100]
+		if (is_prime(i)) prime.push_back(i);	// add new prime to vector
+
+	cout << "Primes: ";
+	for (int i = 0; i < prime.size(); i++)
+		cout << prime[i] << endl;
+}
