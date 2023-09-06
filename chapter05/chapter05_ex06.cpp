42eb0b05f491f45e6fde25d2f63ea1e7e0232e16
mb305151 <115078322+mb305151@users.noreply.github.com>
2023-09-06 12:44:14 +0200

    Add files via upload

---
+++ chapter05/chapter05_ex06.cpp
@@ -0,0 +1,48 @@
+﻿
+#include <iostream>
+using namespace std;
+
+double ctof(double c) {
+    double f = (c * 1.8) + 32;
+    return f;
+}
+
+double ftoc(double f) {
+    double c = (f - 32) * 0.55556;
+    return c;
+}
+
+
+
+int main()
+{
+    cout << "Choose '1' to convert Celsius -> Fahrenheit, '2' to convert Fahrenheit -> Celsius" << endl;
+    int pick;
+    cin >> pick;
+
+    if (pick == 1) {
+        double c;
+        cin >> c;
+        cout << endl;
+        double f = ctof(c);
+        cout << f;
+    }
+    else {
+        double f;
+        cin >> f;
+        cout << endl;
+        double c = ftoc(f);
+        cout << c;
+    }
+
+
+
+   
+
+
+
+
+
+
+}
+
