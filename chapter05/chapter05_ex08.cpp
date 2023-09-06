75be4175ad0825e069768e56e88087caecb4a89c
mb305151 <115078322+mb305151@users.noreply.github.com>
2023-09-06 13:40:01 +0200

    Add files via upload

---
+++ chapter05/chapter05_ex08.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int amountOfNumbersToSum;
+    vector <int> numbers;
+    int number;
+    int sum = 0;
+
+    cout << "How many numbers do you want to sum?\n";
+    cin >> amountOfNumbersToSum;
+    cout << "write down the numbers, end with |\n";
+
+
+    while (cin >> number) {
+        numbers.push_back(number);
+    }
+    
+  
+
+    for (int i = 0; i < amountOfNumbersToSum; i++) {
+        sum += numbers[i];
+        
+    }
+    cout << sum;
+
+
+
+}
+
