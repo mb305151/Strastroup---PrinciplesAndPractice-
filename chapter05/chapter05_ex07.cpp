9d4bc2f58927b1509fbc9b4d2b816d30d7986183
mb305151 <115078322+mb305151@users.noreply.github.com>
2023-09-06 13:03:21 +0200

    Add files via upload

---
+++ chapter05/chapter05_ex07.cpp
@@ -0,0 +1,42 @@
+﻿// SOLVING ZEROS OF A LINEAR FUNCTIONS
+#include <iostream>
+using namespace std;
+
+int sqrtCalc(int a, int b, int c) {
+    double mySqrt = sqrt((b * b) - (4*a*c));
+    return mySqrt;
+}
+
+int main()
+{
+    int a, b, c;
+    double x, x1, x2;
+
+    cout << "Import a , b , c\n";
+    cin >> a >> b >> c;
+    double mySqrt = sqrtCalc(a, b, c);
+
+    x = (- b - mySqrt) / (2 * a);
+    x1 = (-b - mySqrt) / (2 * a);
+    x2= (-b + mySqrt) / (2 * a);
+
+
+    if (mySqrt > 0) {
+        cout << "x1 = " << x1 << endl;
+        cout << "x2 = " << x2 << endl;
+    }
+    else if (mySqrt = 0) {
+        cout << "x = " << x << endl;
+    }
+    else {
+        cout << "no zero places";
+    }
+
+
+
+
+
+
+
+
+}
